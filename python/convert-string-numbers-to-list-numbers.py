ad29ad250e3d1d4e78e1acd0fe68fa5ffc648628
vic4key <vic4key@gmail.com>
2020-07-01 18:59:17 +0700

    python : random a list of numbers & convert a string-numbers to a list of numbers

---
+++ python/convert-string-numbers-to-list-numbers.py
@@ -0,0 +1,3 @@
+s = "16 09 19 91"
+l = list(map(int, s.split(' ')))
+print(l)
---
+++ python/random-list-numbers.py
@@ -0,0 +1,4 @@
+import random as rd
+
+l = rd.sample(range(0, 100), 5) # random a list of 5 numbers that in range [0, 100]
+print(l)
