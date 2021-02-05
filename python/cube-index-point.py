65814896b3afd2d6f05c71a4f3cc588a29ba9655
Vic P <vic4key@gmail.com>
2021-02-05 20:02:00 +0700

    python : cube index and point conversion

--- python/cube-index-point.py
+++ python/cube-index-point.py
@@ -2,17 +2,34 @@
 For cube, convert a point to index and reverse index to a point
 '''
 
-def index_to_point(index, nrow, ncol):
+# 2D
+
+def point_to_index_2d(p, ncol):
+	x, y = p
+	return y + x * ncol
+
+def index_to_point_2d(index, ncol):
+	x, y = divmod(index, ncol)
+	return (x, y)
+
+nrow, ncol = 3, 4
+index = point_to_index_2d((2, 2), ncol)
+point = index_to_point_2d(index, ncol)
+print(index, point)
+
+# 3D
+
+def index_to_point_3d(index, nrow, ncol):
 	z, _ = divmod(index, nrow * ncol)
 	x, y = divmod(_, ncol)
 	return (x, y, z)
 
-def point_to_index(p, nrow, ncol):
+def point_to_index_3d(p, nrow, ncol):
 	x, y, z = p
 	return y + x * ncol + z * nrow * ncol
 
-nrow, ncol = 3, 4
+nrow, ncol, ndep = 3, 4, 5
 
-index = point_to_index((2, 1, 3), nrow, ncol)
-point = index_to_point(index, nrow, ncol)
+index = point_to_index_3d((1, 2, 3), nrow, ncol)
+point = index_to_point_3d(index, nrow, ncol)
 print(index, point)
