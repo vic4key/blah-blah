cadb4a952df4e271b9dac7965236f5a3a9cee87a
Vic P <vic4key@gmail.com>
2021-06-07 19:18:41 +0700

    cpp : vtk import/export vtk poly data as vtk file

--- cpp/vtk-import-export-vtk-poly-data-as-vtk.cpp
+++ cpp/vtk-import-export-vtk-poly-data-as-vtk.cpp
@@ -1,5 +1,16 @@
 // import/export vtkPolyData as .vtk
 
+#include <vtkPolyDataReader.h>
+#include <vtkPolyDataWriter.h>
+
+// VTK 9.0
+#pragma comment(lib, "vtkIOCore-9.0.lib")
+#pragma comment(lib, "vtkIOLegacy-9.0.lib")
+
+// VTK 6.0
+// #pragma comment(lib, "vtkIOCore-6.0.lib")
+// #pragma comment(lib, "vtkIOLegacy-6.0.lib")
+
 // https://stackoverflow.com/questions/29796962/how-to-save-a-vtksphere-to-a-vtk-file
 const auto fn_export_vtk_poly_data = [](vtkSmartPointer<vtkPolyData> data, const std::string& file_name) -> void
 {
