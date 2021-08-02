e502706a5e27c2ea7db8da9f42dbf6284a02eade
Vic P <vic4key@gmail.com>
2021-08-02 09:52:55 +0700

    python : create gray/rgb image by put pixel

--- python/create-rgb-image-by-put-pixel.py
+++ python/create-rgb-image-by-put-pixel.py
@@ -2,7 +2,9 @@
 
 '''
       {
-        std::string s = "path\\to\\image.bin";
+        std::string s = "path\\to\\bla-bla\\python\data\\imgray\\"
+        s += "image";
+        s += ".bin";
         auto f = fopen(s.c_str(), "wb");
         fwrite(&image(0), 1, image.bytes(), f);
         fclose(f);
@@ -13,11 +15,15 @@ import random as rd
 from PIL import Image
 from PyVutils import File
 from random import *
-import glob
+import glob, math
 
 # input parameter
 path_dir = RF"data\imgray\*.bin"
-rows, cols = 801, 801 # input parameter
+square = True
+cols, rows = None, None # required if `square = False`
+
+if not square:
+  assert rows and cols is not None, "missing value for number of rows and columns"
 
 color_white = (0xFF, 0xFF, 0xFF)
 color_black = (0x00, 0x00, 0x00)
@@ -26,8 +32,14 @@ for file_path in glob.glob(path_dir):
 
   data = File.Read(file_path)
 
-  rows, cols = 801, 801
+  if square:
+    size = int(math.sqrt(len(data)))
+    rows, cols = size, size
+
+  assert rows*cols == len(data), "data size did not met number of rows and columns"
+
   im = Image.new("RGB", (rows, cols), color_white)
+  print("image =", im, "<%d>" % len(data))
 
   for row in range(0, rows):
     for col in range(0, cols):
