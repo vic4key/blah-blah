4b9ab03706e3617e7f3b10a60236872cd678ab09
Vic P <vic4key@gmail.com>
2022-04-18 23:49:02 +0700

    cpp : rtti - run-time type identification

---
+++ cpp/rtti.cpp
@@ -0,0 +1,45 @@
+auto pObject = new class instance;
+
+auto pBase = PBYTE(module);
+
+auto address_vtable = *reinterpret_cast<ULONG_PTR*>(pObject);
+address_vtable -= sizeof(ULONG_PTR);
+auto address_rtti = *(ULONG_PTR*)(address_vtable);
+
+auto ptr_rtti = reinterpret_cast<_RTTICompleteObjectLocator*>(address_rtti);
+assert(ptr_rtti != nullptr);
+
+auto pTypeDescriptor  = reinterpret_cast<TypeDescriptor*>(pBase + ptr_rtti->pTypeDescriptor);
+auto pClassDescriptor = reinterpret_cast<_RTTIClassHierarchyDescriptor*>(pBase + ptr_rtti->pClassDescriptor);
+
+auto pBaseClassArray = reinterpret_cast<DWORD*>(pBase + pClassDescriptor->pBaseClassArray);
+assert(pBaseClassArray != nullptr);
+
+for (auto i = 0u; i < pClassDescriptor->numBaseClasses; i++)
+{
+	auto pBaseClass = reinterpret_cast<_RTTIBaseClassDescriptor*>(pBase + pBaseClassArray[i]);
+	assert(pBaseClass != nullptr);
+
+	auto pClassDescriptor = reinterpret_cast<_RTTIClassHierarchyDescriptor*>(pBase + pBaseClass->pClassDescriptor);
+	auto pTypeDescriptor = reinterpret_cast<TypeDescriptor*>(pBase + pBaseClass->pTypeDescriptor);
+
+	if (pTypeDescriptor->name != nullptr)
+	{
+		auto class_name = Utils::undecorate_cpp_name(pTypeDescriptor->name);
+
+		vu::msg_debug_A("%p %s `%s`\n", pTypeDescriptor->pVFTable, pTypeDescriptor->name, class_name.c_str());
+
+		// if (vu::contains_string_A(class_name, pobject->name))
+		// {
+		//   // list all methods in the current base class
+		//   // then traversal to its base classes
+		//   // and append all their methods to method list
+		//   for (auto ptr = PULONG_PTR(*(ULONG_PTR*)(pTypeDescriptor->pVFTable)); ptr; ptr++)
+		//   {
+		//     auto address = ULONG_PTR(ptr);
+		//     vu::msg_debug_A("%p\n", address);
+		//   }
+		//   break;
+		// }
+	}
+}
