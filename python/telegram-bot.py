d14dfae73eca25c63bd4c15a9902501072c5bf35
Vic P <vic4key@gmail.com>
2022-12-02 14:04:07 +0700

    python : telegram-bot

--- python/telegram-bot.py
+++ python/telegram-bot.py
@@ -1,9 +1,10 @@
 # https://core.telegram.org/
 # https://www.shellhacks.com/telegram-api-send-message-personal-notification-bot/
+# https://help.nethunt.com/en/articles/6253243-how-to-make-an-api-call-to-the-telegram-channel
 
 # notes:
 # dump json object by forward a chat message to `@JsonDumpBot`
-# the channel id is '-100{<id>-from-url}' # `https://web.telegram.org/k/<id>`
+# the channel id is '-100{<id>-from-url}' # `https://web.telegram.org/k/<id>` (eg. https://web.telegram.org/k/-1642803959)
 # https://gist.github.com/mraaroncruz/e76d19f7d61d59419002db54030ebe35
 
 ''' to create a telegram bot and get its token
@@ -12,26 +13,20 @@
 3. follow the bots instruction to get it done
 '''
 
-''' to get the chat id
-1. send a message to the bot
-2. tun the command `curl https://api.telegram.org/bot<bot_token>/getUpdates`
+''' to get the user/channel id
+1. for user, bot send a message to the user / for channel, after administrator added bot to a channel)
+2. run the command `curl https://api.telegram.org/bot<bot_token>/getUpdates`
 3. the chat id can be found in the response
 '''
 
-channel_name = "@vic4key_test_channel"
-channel_id = "-1001642803959" # https://web.telegram.org/k/-1642803959
+# bot information
 
 bot_name = "@da_Poet_BOT"
 bot_token = ""
-target_id = "1809685164" # the id of user that bot is chatting with (eg. 'vic4key')
 
-# pip install python-telegram-bot
-# 
-# from telegram import Bot
-# bot = Bot(bot_token)
-# bot.send_message(target_id, "halo")
+# send message via requests
 
-def send_to_telegram(message, chat_id=target_id, access_token=bot_token):
+def send_message_to_telegram(message, chat_id, access_token=bot_token):
     import requests
     requests.packages.urllib3.disable_warnings()
     try:
@@ -39,8 +34,21 @@ def send_to_telegram(message, chat_id=target_id, access_token=bot_token):
             f'https://api.telegram.org/bot{access_token}/sendMessage',
             json={'chat_id': chat_id, 'text': message, 'parse_mode': 'html'},
             verify=False,
+            timeout=5,
         )
-        print(response.text)
+        print(response.status_code, "\n", response.text, "\n")
     except Exception as e: print(e)
 
-send_to_telegram("<b>halo</b> <i>halo</i> <u>halo</u>")
+user_id = "1809685164" # the user id that bot is chatting with (eg. 'vic4key')
+send_message_to_telegram("<b>halo</b> <i>halo</i> <u>halo</u>", chat_id=user_id)
+
+channel_id = "-1001642803959" # the channel id that bot is chatting with (eg. 'TEST CHANNEL')
+send_message_to_telegram("<b>halo</b> <i>halo</i> <u>halo</u>", chat_id=channel_id)
+
+# send message via python-telegram-bot
+# 
+# pip install python-telegram-bot
+# 
+# from telegram import Bot
+# bot = Bot(bot_token)
+# bot.send_message(chat_id, "halo")
