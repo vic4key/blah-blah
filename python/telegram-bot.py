b0f18f0a34dbd613b27493af9be37801cbc1d0dc
Vic P <vic4key@gmail.com>
2022-12-02 11:12:03 +0700

    python : telegram-bot

---
+++ python/telegram-bot.py
@@ -0,0 +1,44 @@
+# https://core.telegram.org/
+# https://www.shellhacks.com/telegram-api-send-message-personal-notification-bot/
+
+# notes:
+# dump json object by forward a chat message to `@JsonDumpBot`
+# the channel id is '-100{<id>-from-url}' # `https://web.telegram.org/k/<id>`
+# https://gist.github.com/mraaroncruz/e76d19f7d61d59419002db54030ebe35
+
+''' to create a telegram bot and get its token
+1. find `BotFather`
+2. start to talk with `/newbot`
+3. follow the bots instruction to get it done
+'''
+
+''' to get the chat id
+1. send a message to the bot
+2. tun the command `curl https://api.telegram.org/bot<access_token>/getUpdates`
+3. the chat id can be found in the response
+'''
+
+channel_name = "@vic4key_test_channel"
+channel_id = "-1001642803959" # https://web.telegram.org/k/-1642803959
+
+bot_name = "@da_Poet_BOT"
+bot_token = ""
+target_id = "1809685164" # the id of user that bot is chatting with (eg. 'vic4key')
+
+# from telegram import Bot
+# bot = Bot(access_token)
+# bot.send_message(chat_id, "hello world")
+
+def send_to_telegram(message, chat_id=target_id, access_token=bot_token):
+    import requests
+    requests.packages.urllib3.disable_warnings()
+    try:
+        response = requests.post(
+            f'https://api.telegram.org/bot{access_token}/sendMessage',
+            json={'chat_id': chat_id, 'text': message},
+            verify=False,
+        )
+        print(response.text)
+    except Exception as e: print(e)
+
+send_to_telegram("halo")
