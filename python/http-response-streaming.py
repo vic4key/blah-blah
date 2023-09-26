1f9ba6933f32b72b11adc030115dd459d5bec4e6
Vic P <vic4key@gmail.com>
2023-09-26 14:26:49 +0700

    python: http-response-streaming.py

--- python/http-response-streaming.py
+++ python/http-response-streaming.py
@@ -1,14 +1,12 @@
-import requests, time
+import requests, time, random
 
 response = requests.get('https://raw.githubusercontent.com/vic4key/Vutils/master/include/Vutils.h', verify=False)
 assert response.status_code == 200
 
-delay = 0.1
-
 for line in response.iter_lines(decode_unicode=True, delimiter=None):
     print(line)
-    time.sleep(delay)
+    time.sleep(random.randrange(0, 5) / 10)
 
 # for chunk in response.iter_content(chunk_size=100, decode_unicode=True):
 #     print(chunk)
-#     time.sleep(delay)
+#     time.sleep(random.randrange(0, 5) / 10)
