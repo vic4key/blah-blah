ab40e76cbd05c935e51c2e3a68f13d20920bcb0b
Vic P <vic4key@gmail.com>
2023-12-26 18:14:08 +0700

    python: python-data-and-raw-data.py

---
+++ python/python-data-and-raw-data.py
@@ -0,0 +1,76 @@
+# https://docs.python.org/3/library/ctypes.html
+# https://docs.python.org/3/library/ctypes.html#fundamental-data-types
+
+# https://docs.python.org/3/library/struct.html
+# https://docs.python.org/3/library/struct.html#format-characters
+
+
+
+# Pack/Unpack between Python Data Types & Raw Data Bytes
+
+import struct, binascii
+
+b  = b""
+b += struct.pack("Q", 0x1122334455667788) # unsigned long long
+b += struct.pack("L", 0xAABBCCDD) # unsigned long
+b += struct.pack("H", 0xEEFF) # unsigned long
+print(b) # <class 'bytes'>
+print(binascii.hexlify(b))
+
+'''
+b'\x88wfUD3"\x11\xdd\xcc\xbb\xaa\xff\xee'
+b'8877665544332211ddccbbaaffee'
+'''
+
+l = list(struct.unpack("QLH", b))
+print([hex(e) for e in l])
+
+'''
+['0x1122334455667788', '0xaabbccdd', '0xeeff']
+'''
+
+
+
+import ctypes, ctypes.util
+
+# Create a raw data buffer (C's bytes)
+s = ctypes.create_string_buffer(b) # <class 'ctypes.c_char_Array_15'>
+print(s) # <class 'bytes'>
+
+'''
+<ctypes.c_char_Array_15 object at 0x000001F4049DF9C8>
+'''
+
+# Calculate size in bytes of a C data type
+print("ctypes.c_void_p =", ctypes.sizeof(ctypes.c_void_p))
+
+'''
+ctypes.c_void_p = 8
+'''
+
+# Find the raw address of an exported C-function 
+
+user32 = ctypes.CDLL(ctypes.util.find_library("user32"))
+
+temp_1 = user32.MessageBoxA # <class 'ctypes.CDLL.__init__.<locals>._FuncPtr'>
+print("Python Object (_FuncPtr):", temp_1)
+
+temp_2 = ctypes.cast(ctypes.byref(temp_1), ctypes.POINTER(ctypes.c_void_p)) # <class 'LP_c_void_p'>     Eg. void* temp_2 = &MessageBoxA
+print("Python Object (LP_c_void_p):", temp_2)
+
+temp_3 = temp_2.contents.value # <class 'int'>      Eg. auto temp_3 = *temp_2;
+print("Raw Function Pointer of `MessageBoxA` at", hex(temp_3))
+
+'''
+Python Object (_FuncPtr): <_FuncPtr object at 0x00000141381C8D48>
+Python Object (LP_c_void_p): <__main__.LP_c_void_p object at 0x00000141381EF6C8>
+Raw Function Pointer of `MessageBoxA` at 0x7ffd5aec1d40 # This is actual address of MessageBoxA function in memory
+'''
+
+# Invoke a C-function by raw-address of the function from Python
+MessageBoxA_C_Prototype = ctypes.CFUNCTYPE( # <class '_ctypes.PyCFuncPtrType'>
+    ctypes.c_int, # return
+    ctypes.c_void_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_ulong # arguments
+)
+MessageBoxA = MessageBoxA_C_Prototype(temp_3) # <class 'ctypes.CFUNCTYPE.<locals>.CFunctionType'> # Cast raw-address to Python ctypes 
+MessageBoxA(0, b"text", b"title", 0) # Invoke the function
