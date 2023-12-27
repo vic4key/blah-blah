9a3bf8ad545f470dfcb873888b87395060844036
Vic P <vic4key@gmail.com>
2023-12-27 13:20:42 +0700

    python: invoke_c_function.py

--- python/export_c_function.cpp
+++ python/export_c_function.cpp
@@ -1,3 +1,7 @@
+#include <stdio.h>
+
+#pragma optimize("", off)
+
 extern "C" __declspec(dllexport) void change_ptr_var(int* pvar)
 {
   if (pvar != nullptr)
@@ -10,3 +14,15 @@ extern "C" __declspec(dllexport) void change_ref_var(int& var)
 {
   var = 456;
 }
+
+extern "C" __declspec(dllexport) void print_message(const char* message)
+{
+  printf("%s\n", message);
+}
+
+extern "C" __declspec(dllexport) void c_invoke_print_message()
+{
+  print_message("This is a string from C code");
+}
+
+#pragma optimize("", on)
--- python/invoke_c_function.py
+++ python/invoke_c_function.py
@@ -28,3 +28,17 @@ print(var.value)
 123
 456
 '''
+
+
+
+# invoke a python function with c syntax
+
+@ctypes.CFUNCTYPE(None, ctypes.c_char_p)
+def print_message(message):
+    msg = message.decode('utf-8')
+    print(f"Invoked `print_message('{msg}')`")
+
+pfn_c_print_message = ctypes.cast(ctypes.byref(print_message), ctypes.POINTER(ctypes.c_void_p))
+print_message_c_prototype = ctypes.CFUNCTYPE(None, ctypes.c_char_p)
+c_print_message = print_message_c_prototype(pfn_c_print_message.contents.value)
+c_print_message(b"This is a string from Python code with C syntax")
