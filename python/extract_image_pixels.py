0c8def8e8650d04dbe11cfbf308af2b81b3961b5
Vic P <vic4key@gmail.com>
2024-11-18 00:06:32 +0700

    python: extract_image_pixels.py

--- python/extract_image_pixels.py
+++ python/extract_image_pixels.py
@@ -3,20 +3,19 @@
 import sys
 from PIL import Image
 
-def save_pixel_data_to_binary(image_path, binary_file_path):
+def extract_and_save_pixels_to_binary(image_path, binary_file_path):
     with Image.open(image_path) as img:
         pixel_data = img.tobytes()
-        with open(binary_file_path, 'wb') as binary_file:
-            binary_file.write(pixel_data)
-
+        with open(binary_file_path, "wb") as f:
+            f.write(pixel_data)
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print("Usage: python extract_image_pixels.py <path/to/image>")
 		sys.exit(0)
 
-	image_path = sys.argv[1]
-	binary_file_path = image_path + ".bin"  # Change this to your desired binary file path
+	image_path  = sys.argv[1]
+	binary_path = image_path + ".bin"
 
-	save_pixel_data_to_binary(image_path, binary_file_path)
-	print(f"Pixel data saved to {binary_file_path}")
+	extract_and_save_pixels_to_binary(image_path, binary_path)
+	print(f"the image pixels saved to '{binary_path}'")
